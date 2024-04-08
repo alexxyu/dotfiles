bbd75484f8c782b4ee93aeddf2819669eeb48e0f
Alex Yu <yu.alexanderx@gmail.com>
2024-04-07 17:38:06 -0700

    Create generic MacOS setup script

---
+++ install/macos/setup.sh
@@ -0,0 +1,29 @@
+#################
+# Initial setup #
+#################
+sudo chown -R $(whoami):admin /usr/local
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew doctor
+brew update
+
+#########################
+# Install brew packages #
+#########################
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+brew bundle --file="$parent_path/../../homebrew/Brewfile"
+
+##################################
+# Install any extra dependencies #
+##################################
+
+# Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# bpython
+pip install bpython
+
+################################
+# Set zsh as the default shell #
+################################
+chsh -s /bin/zsh
+
