5274e853a0786e650043b164f6de928ef9147e36
Alex Yu <yu.alexanderx@gmail.com>
2024-04-16 22:15:03 -0700

    Fix typos in install script

--- install/setup.sh
+++ install/setup.sh
@@ -22,16 +22,16 @@ mv ~/.zshrc $BACKUP_DIR 2>/dev/null
 
 # Create symlinks to dotfiles in repo
 REPO_DIR=$(git rev-parse --show-toplevel)
-cd REPO_DIR
+cd $REPO_DIR
 stow -t ~ git
 stow -t ~ vim
 stow -t ~ zsh
 
 # Setup git-credential-manager now since the gitconfig got overridden
 if [ "$(uname)" = "Linux" ]; then
-    git-credential-manager configure
     # This is a hacky way to set the config in the .local/.gitconfig
-    HOME=$HOME/.local/ git config --global credential.credentialStore gpg
+    HOME=$HOME/.local git-credential-manager configure
+    HOME=$HOME/.local git config --global credential.credentialStore gpg
 fi
 
 echo "Successfully linked dotfiles to this repo! Your old dotfiles have been moved to $BACKUP_DIR."
