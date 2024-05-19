bc7c0daf69c6129abbbca909d5d8db8a8d10d98d
Alex Yu <yu.alexanderx@gmail.com>
2024-05-19 19:06:43 +0000

    Set up bat module

---
+++ home/shell/bat.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  ...
+}: {
+  config = {
+    programs.bat = {
+      enable = true;
+
+      themes = {
+        Catppuccin-latte = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "d714cc1d358ea51bfc02550dabab693f70cccea0";
+            sha256 = "Q5B4NDrfCIK3UAMs94vdXnR42k4AXCqZz6sRn8bzmf4=";
+          };
+          file = "themes/Catppuccin Latte.tmTheme";
+        };
+      };
+    };
+
+    home.shellAliases = {
+      cat = "bat -p";
+      less = "bat";
+    };
+
+    home.sessionVariables = {
+      LESS = "-IFSR --mouse";
+    };
+  };
+}
--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
+    ./bat.nix
     ./dua.nix
     ./zsh.nix
   ];
