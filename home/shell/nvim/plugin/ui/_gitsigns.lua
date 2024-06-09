0bb9e6df3cd1cf2fadae6ce42f7a3e0e9baa5a15
Alex Yu <yu.alexanderx@gmail.com>
2024-06-08 22:58:44 -0700

    Update gitsigns keymaps

--- home/shell/nvim/plugin/ui/_gitsigns.lua
+++ home/shell/nvim/plugin/ui/_gitsigns.lua
@@ -14,6 +14,38 @@ require('gitsigns').setup {
     delay = 200,
     ignore_whitespace = true,
   },
-}
+  on_attach = function(bufnr)
+    local gitsigns = require('gitsigns')
 
-vim.keymap.set('n', '<leader>bl', ':Gitsigns toggle_current_line_blame<CR>', {})
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map('n', ']h', function()
+      if vim.wo.diff then
+        vim.cmd.normal({ ']h', bang = true })
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    map('n', '[h', function()
+      if vim.wo.diff then
+        vim.cmd.normal({ '[h', bang = true })
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    map('n', '<leader>hs', gitsigns.stage_hunk)
+    map('n', '<leader>hr', gitsigns.reset_hunk)
+    map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+    map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+    map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+
+    map('n', '<leader>hp', gitsigns.preview_hunk)
+    map('n', '<leader>hb', ':Gitsigns toggle_current_line_blame<CR>')
+  end
+}
