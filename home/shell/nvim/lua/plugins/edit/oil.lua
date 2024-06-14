0ffa0e6081233f4b9a16318ae2b8bc0ded0556da
Alex Yu <yu.alexanderx@gmail.com>
2024-06-13 20:52:25 -0700

    Add oil.nvim

---
+++ home/shell/nvim/lua/plugins/edit/oil.lua
@@ -0,0 +1,16 @@
+return {
+  'stevearc/oil.nvim',
+  config = function()
+    require('oil').setup({
+      columns = {
+        "icon",
+        "permissions",
+      },
+      delete_to_trash = true,
+    })
+
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+};
