582c2e84180c22de25bd23cac1b1bee555985d57
Alex Yu <yu.alexanderx@gmail.com>
2024-06-16 14:21:09 -0700

    Fix indentline scope

--- home/shell/nvim/lua/plugins/ui/indentline.lua
+++ home/shell/nvim/lua/plugins/ui/indentline.lua
@@ -1,5 +1,12 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
   main = "ibl",
-  opts = {},
+  opts = {
+    scope = {
+      highlight = { "Label" },
+      show_start = false,
+      show_end = false,
+    },
+  },
 };
