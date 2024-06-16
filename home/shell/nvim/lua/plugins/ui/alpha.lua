60b1ee30d40ba9b398050857deb6aed88266421c
Alex Yu <yu.alexanderx@gmail.com>
2024-06-16 15:56:01 -0700

    Fix alpha-nvim alignment

--- home/shell/nvim/lua/plugins/ui/alpha.lua
+++ home/shell/nvim/lua/plugins/ui/alpha.lua
@@ -23,7 +23,7 @@ return {
       dashboard.button('r', '  > Recent', '<cmd>Telescope oldfiles<cr>'),
       dashboard.button('-', '  > File Explorer', '<cmd>Oil<cr>'),
       dashboard.button('l', '󰒲  > Lazy', '<cmd>Lazy<cr>'),
-      dashboard.button('m', '󱌣 > Mason', '<cmd>Mason<cr>'),
+      dashboard.button('m', '󱌣  > Mason', '<cmd>Mason<cr>'),
       dashboard.button('q', '󰩈  > Quit', '<cmd>qa<cr>'),
     }
 
