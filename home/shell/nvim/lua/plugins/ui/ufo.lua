a5044a6b6ea15a790c47659b4443eacb48b80380
Alex Yu <yu.alexanderx@gmail.com>
2024-06-16 14:55:50 -0700

    Add nvim-ufo

---
+++ home/shell/nvim/lua/plugins/ui/ufo.lua
@@ -0,0 +1,80 @@
+return {
+  'kevinhwang91/nvim-ufo',
+  dependencies = {
+    'kevinhwang91/promise-async',
+    {
+      'luukvbaal/statuscol.nvim',
+      config = function()
+        local builtin = require('statuscol.builtin')
+        require('statuscol').setup(
+          {
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc },      click = 'v:lua.ScFa' },
+              { text = { '%s' },                  click = 'v:lua.ScSa' },
+              { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' }
+            }
+          }
+        )
+      end,
+    }
+  },
+  init = function()
+    vim.o.foldcolumn = '1' -- '0' is not bad
+    vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+  end,
+  opts = {
+    preview = {
+      win_config = {
+        winblend = 0,
+      },
+    },
+    provider_selector = function(bufnr, filetype, buftype)
+      return { 'treesitter', 'indent' }
+    end,
+    fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+      local newVirtText = {}
+      local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+      local sufWidth = vim.fn.strdisplaywidth(suffix)
+      local targetWidth = width - sufWidth
+      local curWidth = 0
+      for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+          table.insert(newVirtText, chunk)
+        else
+          chunkText = truncate(chunkText, targetWidth - curWidth)
+          local hlGroup = chunk[2]
+          table.insert(newVirtText, { chunkText, hlGroup })
+          chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          -- str width returned from truncate() may less than 2nd argument, need padding
+          if curWidth + chunkWidth < targetWidth then
+            suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+          end
+          break
+        end
+        curWidth = curWidth + chunkWidth
+      end
+      table.insert(newVirtText, { suffix, 'MoreMsg' })
+      return newVirtText
+    end,
+  },
+  config = function(_, opts)
+    local ufo = require('ufo')
+    ufo.setup(opts)
+
+    vim.keymap.set('n', 'zR', ufo.openAllFolds)
+    vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+    vim.keymap.set('n', 'zr', ufo.openFoldsExceptKinds)
+    vim.keymap.set('n', 'zp', function()
+      local winid = ufo.peekFoldedLinesUnderCursor()
+      if not winid then
+        vim.lsp.buf.hover()
+      end
+    end, { desc = 'Preview fold' })
+  end
+}
