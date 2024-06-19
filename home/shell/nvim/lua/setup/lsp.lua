c5033d9f2fc45269253bf20f4e912bc2e23d05c0
Alex Yu <yu.alexanderx@gmail.com>
2024-06-19 10:59:56 -0700

    Change to basedpyright lsp

--- home/shell/nvim/lua/setup/lsp.lua
+++ home/shell/nvim/lua/setup/lsp.lua
@@ -23,16 +23,13 @@ local servers = {
   },
 
   ruff_lsp = {},
-  pyright = {
+  basedpyright = {
     settings = {
-      pyright = {
+      basedpyright = {
         -- Using Ruff's import organizer
         disableOrganizeImports = true,
-      },
-      python = {
         analysis = {
-          -- Ignore all files for analysis to exclusively use Ruff for linting
-          ignore = { '*' },
+          typeCheckingMode = 'standard',
         },
       },
     },
