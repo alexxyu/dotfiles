c0c626f98835751d9619fd625b4d0605e1eba51c
Alex Yu <yu.alexanderx@gmail.com>
2024-06-20 20:47:08 -0700

    Add GoTest keymaps for gopher

--- home/shell/nvim/lua/plugins/lang/gopher.lua
+++ home/shell/nvim/lua/plugins/lang/gopher.lua
@@ -5,5 +5,10 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
   },
-  opts = {},
+  config = function()
+    require('gopher').setup({})
+
+    vim.keymap.set('n', '<leader>tg', '<cmd>GoTestAdd<cr>', { desc = 'Generate test for function' })
+    vim.keymap.set('n', '<leader>tG', '<cmd>GoTestsAll<cr>', { desc = 'Generate test for file' })
+  end,
 }
