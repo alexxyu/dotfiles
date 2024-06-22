a96253159703c27dffcc9003fd7d300f8db4b196
Alex Yu <yu.alexanderx@gmail.com>
2024-06-21 20:30:02 -0700

    Add snippets

--- home/shell/nvim/lua/plugins/edit/cmp.lua
+++ home/shell/nvim/lua/plugins/edit/cmp.lua
@@ -6,6 +6,7 @@ return {
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/cmp-path' },
     { 'SergioRibera/cmp-dotenv' },
+    { 'saadparwaiz1/cmp_luasnip' },
   },
   event = 'VeryLazy',
   config = function()
